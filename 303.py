4c2127eccc84f9506a268e87f8765409d11f851b
wds2006sdo <641614152@qq.com>
2016-05-25 01:34:02 +0800

    303

---
+++ 303.py
@@ -0,0 +1,24 @@
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.dp = [0]
+        for i in range(len(nums)):
+            self.dp.append(self.dp[i]+nums[i])
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.dp[j+1] - self.dp[i]
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)
